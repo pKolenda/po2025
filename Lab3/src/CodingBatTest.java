ebf47b559d7b59f9ac057b4082ce03614f7c1ba8
Piotr <piotr@kolenda.com>
2025-10-20 19:11:43 +0200

    Skońcenie lab3 w domu

--- Lab3/src/CodingBatTest.java
+++ Lab3/src/CodingBatTest.java
@@ -7,14 +7,21 @@ public class CodingBatTest {
     @Test
     public void monkeyTrouble() {
 
-        assertTrue(new CodingBat().monkeyTrouble(true,true));
-        assertTrue(new CodingBat().monkeyTrouble(false,false));
-        assertFalse(new CodingBat().monkeyTrouble(false,true));
-        assertFalse(new CodingBat().monkeyTrouble(true,false));
+        assertTrue(new CodingBat().monkeyTrouble(true, true));
+        assertTrue(new CodingBat().monkeyTrouble(false, false));
+        assertFalse(new CodingBat().monkeyTrouble(false, true));
+        assertFalse(new CodingBat().monkeyTrouble(true, false));
     }
 
     @Test
     public void firstHalf() {
+        assertEquals("Woo", new CodingBat().firstHalf("WooHoo"));
+        assertEquals("Hello", new CodingBat().firstHalf("HelloThere"));
+        assertEquals("abc", new CodingBat().firstHalf("abcdef"));
+        assertEquals("a", new CodingBat().firstHalf("ab"));
+        assertEquals("", new CodingBat().firstHalf(""));
+        assertEquals("01234", new CodingBat().firstHalf("0123456789"));
+        assertEquals("kit", new CodingBat().firstHalf("kitten"));
 
 
     }
@@ -22,12 +29,12 @@ public class CodingBatTest {
     @Test
     public void countEvens() {
 
-        assertEquals(2, new CodingBat().countEvens(19));
-        assertEquals(11, new CodingBat().countEvens(10));
-        assertEquals(0, new CodingBat().countEvens(21));
-        assertEquals(8, new CodingBat().countEvens(25));
-        assertEquals(18, new CodingBat().countEvens(30));
-        assertEquals(21, new CodingBat().countEvens(0));
+        assertEquals(3, new CodingBat().countEvens(new int[]{2, 1, 2, 3, 4}));
+        assertEquals(3, new CodingBat().countEvens(new int[]{2, 2, 0}));
+        assertEquals(0, new CodingBat().countEvens(new int[]{1, 3, 5}));
+        assertEquals(0, new CodingBat().countEvens(new int[]{}));
+        assertEquals(1, new CodingBat().countEvens(new int[]{11, 9, 0, 1}));
+        assertEquals(2, new CodingBat().countEvens(new int[]{2, 11, 9, 0}));
 
 
     }
